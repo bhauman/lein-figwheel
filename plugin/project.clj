c25979af8f4c17a0cbf0dbe724557f098a2f087d
Bruce Hauman <bhauman@gmail.com>
2014-05-08 12:52:05 -0400

    updating project files

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,9 +1,14 @@
-(defproject lein-figwheel "0.1.0-SNAPSHOT"
+(defproject lein-figwheel "0.1.1"
   :description "ClojureScript Autobuilder/Server which pushes changed files to the browser."
   :url "https://github.com/bhauman/lein-figwheel"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljsbuild "1.0.3"]]
   :dependencies [[cljsbuild "1.0.3"]
-                 [figwheel "0.1.0-SNAPSHOT"]]
+                 [figwheel "0.1.1"]]
+  :scm { :name "git"
+         :url "https://github.com/bhauman/lein-figwheel"}
   :eval-in-leiningen true)
+
+
+
--- support/project.clj
+++ support/project.clj
@@ -1,14 +1,19 @@
-(defproject figwheel "0.1.0-SNAPSHOT"
+(defproject figwheel "0.1.1"
   :description "ClojureScript Autobuilder/Server which pushes changed files to the browser."
+  :url "https://github.com/bhauman/lein-figwheel"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm { :name "git"
+        :url "https://github.com/bhauman/lein-figwheel"}  
   :dependencies
-    [[org.clojure/clojure "1.5.1"]
-     [org.clojure/clojurescript "0.0-2202"
-      :exclusions [org.apache.ant/ant]]
-     [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-     ;; devserver
-     [fs "1.1.2"]
-     [ring "1.2.1"] 
-     [http-kit "2.1.16"]
-     [compojure "1.1.6"]
-     [watchtower "0.1.1"]
-     [digest "1.4.3"]])
+  [[org.clojure/clojure "1.5.1"]
+   [org.clojure/clojurescript "0.0-2202"
+    :exclusions [org.apache.ant/ant]]
+   [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+   ;; devserver
+   [fs "1.1.2"]
+   [ring "1.2.1"] 
+   [http-kit "2.1.16"]
+   [compojure "1.1.6"]
+   [watchtower "0.1.1"]
+   [digest "1.4.3"]])
