7bf228e8a3b24675a48ea4571f166b7fb3944375
Bruce Hauman <bhauman@gmail.com>
2014-12-10 17:47:35 -0500

    adding dummy file to help upgrade
    A lot of people are going to have :include-macros true
    on their client require statement

---
+++ support/src/figwheel/client.clj
@@ -0,0 +1,7 @@
+(ns figwheel.client)
+
+;; this is a dummy file to be kinder to people
+;; who are upgrading from the older figwheel
+;; the error message is too cryptic
+
+;; I'm sure I'm going to have some macros in here someday :)
