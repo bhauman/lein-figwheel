9beff1038f5eafe03b6da80f9b6eecbec8f77597
Bruce Hauman <bhauman@gmail.com>
2014-12-14 11:30:52 -0500

    cleaning up some debug info

--- support/src/figwheel/auto_builder.clj
+++ support/src/figwheel/auto_builder.clj
@@ -16,40 +16,6 @@
                (fn [f] (.lastModified f)))
                (map :source-file files)))))
 
-(comment
-
-  (def options {
-                :output-to "resources/public/js/compiled/example.js"
-                :output-dir "resources/public/js/compiled/out"
-                  :optimizations :none
-                  :warnings true
-                })
-
-  (def fig-opts {
-                :output-to "resources/public/js/compiled/example.js"
-                :output-dir "resources/public/js/compiled/out"
-
-                 :resource-paths ["/Users/brucehauman/workspace/lein-figwheel/example/dev-resources"
-                                  "/Users/brucehauman/workspace/lein-figwheel/example/resources"
-                                  "/Users/brucehauman/workspace/lein-figwheel/example/other_resources"],
-                 ;; these aren't needed anymore
-                 :js-dirs ["resources/public/js/compiled/out" "resources/public/js/compiled/example.js"],
-                 :name "figwheel-example",
-                 :server-port 3449,
-
-                 :open-file-command "emacsclient",
-                 :root "/Users/brucehauman/workspace/lein-figwheel/example",
-                 :version "0.1.7-SNAPSHOT",
-                 :css-dirs ["resources/public/css"],
-                 :http-server-root "public"})
-  
-  (get-dependency-mtimes ["src"] options)
-
-
-  (autobuild ["src"] options fig-opts)
-  
-  )
-
 (def server-kill-switch (atom false))
 
 (defn autobuild [src-dirs build-options figwheel-options]
@@ -62,9 +28,10 @@
         warning-handlers (conj cljs.analyzer/*cljs-warning-handlers* warning-handler)
         compiler-env    (cljs.env/default-compiler-env build-options)]
     (reset! server-kill-switch (:http-server figwheel-state'))
-    (p/pprint src-dirs)
-    (p/pprint build-options)
-    (p/pprint figwheel-options)
+
+    #_(p/pprint src-dirs)
+    #_(p/pprint build-options)
+    #_(p/pprint figwheel-options)
 
     (loop [dependency-mtimes {}]
       (let [new-mtimes (get-dependency-mtimes src-dirs build-options)]
@@ -79,10 +46,4 @@
               (fig/compile-error-occured figwheel-state' e))))
         (fig/check-for-css-changes figwheel-state')
         (Thread/sleep 100)
-        #_(fig/stop-server figwheel-state')
         (recur new-mtimes)))))
-
-(comment
-  (require 'figwheel.auto-builder)
-  (in-ns 'figwheel.auto-builder)
-  )
