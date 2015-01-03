6badef3c6bfc344a918a0918ee325998b3729648
Bruce Hauman <bhauman@gmail.com>
2015-01-03 11:40:19 -0500

    handle multiple builds

--- plugin/src/leiningen/figwheel.clj
+++ plugin/src/leiningen/figwheel.clj
@@ -44,15 +44,12 @@
            (System/exit 1))))
      requires)))
 
-;; All I can say is I'm sorry about this but, it seems to be the best
-;; way for me to reuse cljsbuild.
-
 ;; this is really deviating from cljsbuild at this point.
 ;; need to dig in and probably rewrite this
 
 ;; need to get rid of crossovers soon
 
-(defn run-compiler [project {:keys [crossover-path crossovers builds]} live-reload-options]
+(defn run-compiler [project {:keys [crossover-path crossovers builds]} figwheel-options]
   (println "Compiling ClojureScript.")
   ; If crossover-path does not exist before eval-in-project is called,
   ; the files it contains won't be classloadable, for some reason.
@@ -60,7 +57,7 @@
     (println "\033[31mWARNING: lein-cljsbuild crossovers are deprecated, and will be removed in future versions.\n
 See https://github.com/emezeske/lein-cljsbuild/blob/master/doc/CROSSOVERS.md for details.\033[0m")
     (fs/mkdirs crossover-path))
-  (let [parsed-builds (list (config/parse-notify-command (first builds)))]
+  (let [parsed-builds (map config/parse-notify-command builds)]
     (run-local-project project crossover-path parsed-builds
      '(require 'cljsbuild.crossover 'cljsbuild.util 'clj-stacktrace.repl 'figwheel-sidecar.auto-builder)
      `(do
@@ -71,10 +68,8 @@ See https://github.com/emezeske/lein-cljsbuild/blob/master/doc/CROSSOVERS.md for
           (when (not-empty '~crossovers)
             (copy-crossovers#)
             (cljsbuild.util/once-every-bg 1000 "copying crossovers" copy-crossovers#))
-          (let [build# (first '~parsed-builds)]
-            (figwheel-sidecar.auto-builder/autobuild (:source-paths build#)
-                                             (:compiler build#)
-                                             ~live-reload-options)))))))
+          (figwheel-sidecar.auto-builder/autobuild* '~parsed-builds
+                                                    ~figwheel-options))))))
 
 (defn optimizations-none?
   "returns true if a build has :optimizations set to :none"
@@ -99,7 +94,8 @@ See https://github.com/emezeske/lein-cljsbuild/blob/master/doc/CROSSOVERS.md for
 
 (defn output-dir-in-resources-root?
   "Check if the build output directory is in or below any of the configured resources directories."
-  [{:keys [output-dir resource-paths http-server-root] :as opts}]
+  [{:keys [output-dir] :as build-options}
+   {:keys [resource-paths http-server-root] :as opts}]
   (and output-dir
        (first (filter (fn [x] (.startsWith output-dir (str x "/" http-server-root)))
                       (relativize-resource-paths resource-paths)))))
@@ -113,49 +109,56 @@ See https://github.com/emezeske/lein-cljsbuild/blob/master/doc/CROSSOVERS.md for
     (vec (map (fn [[k v]] (assoc v :id (name k))) builds))
     builds))
 
-(defn narrow-to-one-build* 
-  "Filters builds to the chosen build-id or if no build id specified returns the first
+(defn narrow-builds* 
+  "Filters builds to the chosen build-ids or if no build-ids specified returns the first
    build with optimizations set to none."
-  [ builds build-id]
-  (let [builds (map-to-vec-builds builds)]
-    (vector
-     (if build-id
-       (some #(and (= (:id %) build-id) %) builds)
-       (first (filter optimizations-none? builds))))))
+  [builds build-ids]
+  (let [builds (map-to-vec-builds builds)
+        ;; ensure string ids
+        builds (map #(update-in % [:id] name) builds)
+        build-ids (set build-ids)]
+    (vec
+     (keep identity
+           (if-not (empty? build-ids)
+             (filter #(get build-ids (:id %)) builds)
+             [(first (filter optimizations-none? builds))])))))
 
 ;; we are only going to work on one build
 ;; still need to narrow this to optimizations none
-(defn narrow-to-one-build
+(defn narrow-builds
   "Filters builds to the chosen build-id or if no build id specified returns the first
    build with optimizations set to none."
-  [project build-id]
-  (update-in project [:cljsbuild :builds] narrow-to-one-build* build-id))
+  [project build-ids]
+  (update-in project [:cljsbuild :builds] narrow-builds* build-ids))
 
 (defn check-for-valid-options
   "Check for various configuration anomalies."
-  [{:keys [builds]} {:keys [http-server-root] :as opts}]
-  (let [build (first builds)
-        opts? (and (not (nil? build)) (optimizations-none? build))
-        out-dir? (output-dir-in-resources-root? opts)]
-    (if (nil? build)
-      (list
-       (str "Figwheel: "
-            "No cljsbuild specified. You may have mistyped the build "
-            "id on the command line or failed to specify a build in "
-            "the :cljsbuild section of your project.clj. You need to have "
-            "at least one build with :optimizations set to :none."))
-      (map
-       #(str "Figwheel Config Error (in project.clj) - " %)
-       (filter identity
-               (list
-                (when-not opts?
-                  "you have build :optimizations set to something other than :none")
-                (when-not out-dir?
-                  (str
-                   (if (:output-dir build)
-                     "your build :output-dir is not in a resources directory."
-                     "you have not configured an :output-dir in your build")
-                   (str "\nIt should match this pattern: " (make-serve-from-display opts))))))))))
+  [{:keys [http-server-root] :as opts} build']
+  (let [build-options (:compiler build')
+        opts? (and (not (nil? build-options)) (optimizations-none? build'))
+        out-dir? (output-dir-in-resources-root? build-options opts)]
+    (map
+     #(str "Figwheel Config Error (in project.clj) - " %)
+     (filter identity
+             (list
+              (when-not opts?
+                "you have build :optimizations set to something other than :none")
+              (when-not out-dir?
+                (str
+                 (if (:output-dir build-options)
+                   "your build :output-dir is not in a resources directory."
+                   "you have not configured an :output-dir in your build")
+                 (str "\nIt should match this pattern: " (make-serve-from-display opts)))))))))
+
+(defn check-config [figwheel-options builds]
+  (if (empty? builds)
+    (list
+     (str "Figwheel: "
+          "No cljsbuild specified. You may have mistyped the build "
+          "id on the command line or failed to specify a build in "
+          "the :cljsbuild section of your project.clj. You need to have "
+          "at least one build with :optimizations set to :none."))
+    (mapcat (partial check-for-valid-options figwheel-options) builds)))
 
 (defn normalize-dir
   "If directory ends with '/' then truncate the trailing forward slash."
@@ -181,24 +184,22 @@ See https://github.com/emezeske/lein-cljsbuild/blob/master/doc/CROSSOVERS.md for
        (apply-to-key vec :css-dirs)
        (apply-to-key vec :resource-paths)))
 
-;; duh!! I should be forcing the optimizations :none option here.
-
 (defn figwheel
   "Autocompile ClojureScript and serve the changes over a websocket (+ plus static file server)."
   [project & build-ids]
-  (let [project (narrow-to-one-build project (first build-ids))
-        current-build (first (get-in project [:cljsbuild :builds]))
+  (let [project (narrow-builds project build-ids)
+        builds (get-in project [:cljsbuild :builds])
         figwheel-options (prep-options
                           (merge
-                           { :output-dir (-> current-build :compiler :output-dir )
-                             :output-to  (-> current-build :compiler :output-to ) }
                            (:figwheel project)
-                           ;; we can get the resource paths from the project
+                           ;; this is for the initial deps files checksums
+                           ;; better to pass all the builds
+                           { :output-dir (get-in (first builds) [:compiler :output-dir]) 
+                             :output-to (get-in (first builds) [:compiler :output-to]) }
                            (select-keys project [:resource-paths])))]
-    (let [errors (check-for-valid-options
-                  (:cljsbuild project)
-                  figwheel-options)]
-      (println (str "Figwheel: focusing on build-id " "'" (:id current-build) "'"))
+    (let [errors (check-config figwheel-options builds)]
+      (println (str "Figwheel: focusing on build-ids ("
+                    (string/join " " (map :id builds)) ")"))
       (if (empty? errors)
         (run-compiler project
                       (config/extract-options project)
