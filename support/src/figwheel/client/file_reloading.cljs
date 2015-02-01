e0022b3979272e1511cbb40e02de86693c2e249a
Bruce Hauman <bhauman@gmail.com>
2015-02-01 11:26:14 -0500

    cleaning up file reloading

--- support/src/figwheel/client/file_reloading.cljs
+++ support/src/figwheel/client/file_reloading.cljs
@@ -13,12 +13,10 @@
 
 (declare add-cache-buster)
 
-(def ^:dynamic *load-from-figwheel* true)
-
 (defn figwheel-closure-import-script [src]
   (if (.inHtmlDocument_ js/goog)
     (do
-      #_(.log js/console "Figwheel: latently loading required file " src )
+      (utils/debug-prn (str "Figwheel: latently loading required file " src ))
       (loader/load (add-cache-buster src))
       true)
     false))
@@ -28,25 +26,14 @@
   (set! (.-CLOSURE_IMPORT_SCRIPT (.-global js/goog)) figwheel-closure-import-script))
 
 ;; this assumes no query string on url
-(defn add-cache-buster [url]
-  (.makeUnique (guri/parse url)))
-
-(defn reload-host [{:keys [websocket-url]}]
-  (-> websocket-url
-      (string/replace-first #"^wss?:" "")
-      (string/replace-first #"^//" "")
-      (string/split #"/")
-      first))
+(defn add-cache-buster [url] (.makeUnique (guri/parse url)))
 
 (defonce ns-meta-data (atom {}))
 
-(defn get-meta-data-for-ns [ns]
-  (get ns-meta-data ns))
+(defn ns-to-js-file [ns] (str (string/replace ns "." "/") ".js"))
 
 (defn resolve-ns [ns]
-  (str (string/replace-first (.-basePath js/goog) "/goog" "")
-       (string/replace ns "." "/")
-       ".js"))
+  (str (.-basePath js/goog) "../" (ns-to-js-file ns)))
 
 (def goog-deps-path (str (.-basePath js/goog) "deps.js"))
 
@@ -58,10 +45,10 @@
   (let [sel  (str "script[src='" goog-deps-path "']")]
     (when-let [el (.querySelector js/document sel)]
       (when-let [el (.-nextElementSibling el)]
-        (.getAttribute el "src")))))
+        (.getAttribute el "src")     ))))
 
 (let [main-file-path (get-main-file-path)]
-  (defn resolve-deps-path [{:keys [file request-url]}]
+  (defn resolve-deps-path [file]
     (cond
       (re-matches #".*goog/deps\.js" file) goog-deps-path
       (and main-file-path
@@ -69,44 +56,53 @@
       main-file-path
       :else (do
               (utils/debug-prn (str "No deps match:" file))
-              request-url))))
+              file))))
 
-(defn resolve-url [{:keys [request-url namespace dependency-file] :as msg}]
-  (if (not *load-from-figwheel*)
+;; server should add a type to each file
+(defn file-type [{:keys [type dependency-file namespace]}]
+    (cond
+      (= type :css)   :css
+      dependency-file :dependency-file
+      namespace       :namespace))
+
+(defmulti resolve-url file-type)
+
+(defmethod resolve-url :default [{:keys [file]}] file)
+
+(defmethod resolve-url :dependency-file [{:keys [file]}]
+  (resolve-deps-path file))
+
+(defmethod resolve-url :namespace [{:keys [namespace]}]
+  (resolve-ns namespace))
+
+(defn reload-file? [{:keys [request-url namespace dependency-file meta-data] :as file-msg}]
+  (and 
+   (or dependency-file
+       (and meta-data (:figwheel-load meta-data))
+       ;; IMPORTANT make sure this file is currently provided
+       (.isProvided_ js/goog (name namespace)))
+   (not (:figwheel-no-load (or meta-data {})))))
+
+(defn js-reload [{:keys [request-url namespace meta-data] :as file-msg} callback]
+  (swap! ns-meta-data assoc namespace meta-data)  
+  (if (reload-file? file-msg)
+    (let [req-url (add-cache-buster request-url)
+          _ (utils/debug-prn (str "FigWheel: Attempting to load " req-url))
+          deferred (loader/load req-url #js { :cleanupWhenDone true })]
+      (.addCallback deferred
+                    #(do
+                       (utils/debug-prn (str "FigWheel: Successfullly loaded " request-url))
+                       (apply callback [(assoc file-msg :loaded-file true)])))
+      (.addErrback deferred
+                   (fn [eb]
+                     (utils/debug-prn (.-stack eb))
+                     (.error js/console "Figwheel: Error loading file with script tag:" request-url))))
     (do
-      (utils/debug-prn "Not loading from figwheel")
-      (if dependency-file
-        (resolve-deps-path msg)
-        (resolve-ns namespace)))
-    request-url))
-
-(defn js-reload [{:keys [request-url namespace dependency-file meta-data] :as msg} callback]
-  (swap! ns-meta-data assoc namespace meta-data)
-  (let [request-url (resolve-url msg)]
-    (if (and
-         (or dependency-file
-             (and meta-data (:figwheel-load meta-data))
-             ;; IMPORTANT make sure this file is currently provided
-           (.isProvided_ js/goog (name namespace)))
-         (not (:figwheel-no-load (or meta-data {}))))
-      (let [req-url (add-cache-buster request-url)
-            _ (utils/debug-prn (str "FigWheel: Attempting to load " req-url))
-            deferred (loader/load req-url #js { :cleanupWhenDone true })]
-        (.addCallback deferred
-                      #(do
-                         (utils/debug-prn (str "FigWheel: Successfullly loaded " request-url))
-                         (apply callback [(assoc msg :loaded-file true)])))
-        (.addErrback deferred
-                     (fn [eb]
-                       (utils/debug-prn (.-stack eb))
-                       (.error js/console "Figwheel: Error loading file with script tag:" request-url))))
-      (do
-        (utils/debug-prn (str "Figwheel: Not trying to load file " request-url))
-        (apply callback [msg])))))
+      (utils/debug-prn (str "Figwheel: Not trying to load file " request-url))
+      (apply callback [file-msg]))))
 
 (defn reload-js-file [file-msg]
   (let [out (chan)]
-    ;; escape context for better error reporting
     (js/setTimeout #(js-reload file-msg (fn [url]
                                           (patch-goog-base)
                                           (put! out url)
@@ -118,11 +114,11 @@
   [files]
   (async/into [] (async/filter< identity (async/merge (mapv reload-js-file files)))))
 
-(defn add-request-url [{:keys [url-rewriter] :as opts} {:keys [file] :as d}]
-  (->> file
-       (str "//" (reload-host opts))
-       url-rewriter
-       (assoc d :request-url)))
+(defn add-request-url [{:keys [url-rewriter] :as opts} {:keys [file] :as file-msg}]
+  (assoc file-msg :request-url
+         (if url-rewriter
+           (url-rewriter file)
+           (resolve-url file-msg))))
 
 (defn add-request-urls [opts files]
   (map (partial add-request-url opts) files))
@@ -130,18 +126,20 @@
 (defn reload-js-files [{:keys [before-jsload on-jsload load-from-figwheel] :as opts} {:keys [files] :as msg}]
   (go
     (before-jsload files)
-    (with-redefs [*load-from-figwheel* load-from-figwheel]
-      (let [files'  (add-request-urls opts files)
-            res'    (<! (load-all-js-files files'))
-            res     (filter :loaded-file res')
-            files-not-loaded  (filter #(not (:loaded-file %)) res')]
-        (when (not-empty res)
-          (.debug js/console "Figwheel: loaded these files")
-          (.log js/console (pr-str (map :file res)))
-          (js/setTimeout #(apply on-jsload [res]) 10))
-        (when (not-empty files-not-loaded)
-          (.debug js/console "Figwheel: NOT loading files that haven't been required")
-          (.log js/console "not required:" (pr-str (map :file files-not-loaded))))))))
+    (let [files'  (add-request-urls opts files)
+          res'    (<! (load-all-js-files files'))
+          res     (filter :loaded-file res')
+          files-not-loaded  (filter #(not (:loaded-file %)) res')]
+      (when (not-empty res)
+        (.debug js/console "Figwheel: loaded these files")
+        (.log js/console (pr-str (map (fn [{:keys [namespace file]}]
+                                        (if namespace
+                                          (ns-to-js-file namespace)
+                                          file)) res)))
+        (js/setTimeout #(apply on-jsload [res]) 10))
+      (when (not-empty files-not-loaded)
+        (.debug js/console "Figwheel: NOT loading files that haven't been required")
+        (.log js/console "not required:" (pr-str (map :file files-not-loaded)))))))
 
 ;; CSS reloading
 
@@ -156,18 +154,15 @@
       (string/replace-first #"^//" "")         
       (string/replace-first #"[^\/]*" "")))
 
-(defn matches-file? [{:keys [file request-url]} link-href]
-  (let [trunc-href (truncate-url link-href)]
-    (or (= file trunc-href)
-        (= (truncate-url request-url) trunc-href))))
-
-(defn matches-file-new?
+(defn matches-file?
   [{:keys [file]} link]
   (when-let [link-href (.-href link)]
-    (let [match-length (count (take-while identity
-                                          (map #(= %1 %2)
-                                               (reverse file)
-                                               (reverse (truncate-url link-href)))))
+    (let [match (string/join "/"
+                         (take-while identity
+                                     (map #(if (= %1 %2) %1 false)
+                                          (reverse (string/split file "/"))
+                                          (reverse (string/split (truncate-url link-href) "/")))))
+          match-length (count match)
           file-name-length (count (last (string/split file "/")))]
       (when (>= match-length file-name-length) ;; has to match more than the file name length
         {:link link
@@ -180,7 +175,7 @@
                   (sort-by
                    (fn [{:keys [match-length current-url-length]}]
                      (- current-url-length match-length))
-                   (keep #(matches-file-new? f-data %)
+                   (keep #(matches-file? f-data %)
                          (current-links))))]
     (:link res)))
 
@@ -212,7 +207,7 @@
 (defn reload-css-file [{:keys [file request-url] :as f-data}]
   (if-let [link (get-correct-link f-data)]
     (add-link-to-doc link (clone-link link (.-href link)))
-    (add-link-to-doc (create-link request-url))))
+    (add-link-to-doc (create-link (or request-url file)))))
 
 (defn reload-css-files [{:keys [on-cssload] :as opts} files-msg]
   (doseq [f (add-request-urls opts (:files files-msg))]
