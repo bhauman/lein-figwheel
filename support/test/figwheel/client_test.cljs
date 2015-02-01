a2e6338a24aa56db22934f1dd157d1b7b7cb53dc
Bruce Hauman <bhauman@gmail.com>
2015-02-01 11:26:47 -0500

    adding the start of testing

---
+++ support/test/figwheel/client_test.cljs
@@ -0,0 +1,7 @@
+(ns figwheel.client-test
+  (:require
+   [figwheel.client :as c]
+   [cljs.test :refer [testing is deftest]]))
+
+(deftest checking
+  (is true))
---
+++ support/test/figwheel/file_reloading_test.cljs
@@ -0,0 +1,7 @@
+(ns figwheel.file-reloading-test
+  (:require
+   [figwheel.client.file-reloading :as f]
+   [cljs.test :refer [testing is deftest]]))
+
+(deftest checking
+  (is true))
