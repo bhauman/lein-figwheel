3e7c86cac8009b0eb5b188cdbec983988cb30118
Bruce Hauman <bhauman@gmail.com>
2015-02-02 18:41:17 -0500

    add conditional assertion support

---
+++ support/src/figwheel/client/utils.clj
@@ -0,0 +1,20 @@
+(ns figwheel.client.utils)
+
+(def dev-blocks? (atom false))
+
+(defmacro enable-dev-blocks! []
+  (reset! dev-blocks? true)
+  `(do))
+
+(defmacro disable-dev-blocks! []
+  (reset! dev-blocks? false)
+  `(do))
+
+(defmacro dev [& body]
+  (if @dev-blocks?
+    `(do ~@body)
+    `(comment ~@body)))
+
+(defmacro dev-assert [& body]
+  `(dev
+     ~@(map (fn [pred-stmt] `(assert ~pred-stmt)) body)))
