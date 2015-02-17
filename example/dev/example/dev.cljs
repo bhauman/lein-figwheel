461cdc609626db445dd063645a42610c2c3a2fc4
Bruce Hauman <bhauman@gmail.com>
2015-02-17 18:25:27 -0500

    demonstrating counter increment pattern

--- example/dev/example/dev.cljs
+++ example/dev/example/dev.cljs
@@ -12,12 +12,10 @@
 (fw/start {
            :websocket-url "ws://localhost:3449/figwheel-ws"
            :build-id "example"
+           ;; if you want to disable autoloading
            ;; :autoload false
            ;; :debug true
            :on-jsload (fn []
-
-                        ;; this is a better way to reload the cube example
-                        ;; which will reload even for non-local changes
-                        ;; (example.cube/stop-and-start-ex3)
-                        )
-           })
+                        ;; it can be helpful to touch the state on 
+                        ;; reload to cause a re-render
+                        (swap! core/app-state update-in [:__figwheel_counter] inc))})
