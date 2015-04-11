d572454a61264c4eb84ec8ae3e8a6b66aab08aa9
Bruce Hauman <bhauman@gmail.com>
2015-04-11 13:27:05 -0400

    bump to 0.2.6-SNAPSHOT

--- sidecar/project.clj
+++ sidecar/project.clj
@@ -1,4 +1,4 @@
-(defproject figwheel-sidecar "0.2.5"
+(defproject figwheel-sidecar "0.2.6-SNAPSHOT"
   :description "ClojureScript Autobuilder/Server which pushes changed files to the browser."
   :url "https://github.com/bhauman/lein-figwheel"
   :license {:name "Eclipse Public License - v 1.0"
@@ -11,7 +11,7 @@
    [org.clojure/clojurescript "0.0-2850"
     :exclusions [org.apache.ant/ant]]
    [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-   [com.cemerick/pomegranate "0.3.0"]   
+   [com.cemerick/pomegranate "0.3.0"]
    [http-kit "2.1.16"]
    [ring-cors "0.1.4"]
    [compojure "1.1.7"]
