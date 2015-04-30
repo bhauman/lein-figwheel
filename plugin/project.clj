07d880e7c3d4955375fd7153b12818d710ec601a
Bruce Hauman <bhauman@gmail.com>
2015-04-30 08:59:55 -0400

    bumping to 0.2.9

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,11 +1,11 @@
-(defproject lein-figwheel "0.2.8"
+(defproject lein-figwheel "0.2.9"
   :description "ClojureScript Autobuilder/Server which pushes changed files to the browser."
   :url "https://github.com/bhauman/lein-figwheel"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[figwheel-sidecar "0.2.8"]
-                 [figwheel "0.2.8"]]
+  :dependencies [[figwheel-sidecar "0.2.9"]
+                 [figwheel "0.2.9"]]
 
   :profiles {
     :dev {
--- support/project.clj
+++ support/project.clj
@@ -1,4 +1,4 @@
-(defproject figwheel "0.2.8"
+(defproject figwheel "0.2.9"
   :description "ClojureScript Autobuilder/Server which pushes changed files to the browser."
   :url "https://github.com/bhauman/lein-figwheel"
   :license {:name "Eclipse Public License - v 1.0"
