d73a3c07cd985f3e09ad083e0e11acdf24e5db04
Vlad Bokov <razum2um@mail.ru>
2015-10-25 04:36:04 +0600

    add eval helper

--- sidecar/src/figwheel_sidecar/channel_server.clj
+++ sidecar/src/figwheel_sidecar/channel_server.clj
@@ -10,6 +10,12 @@
 (defn send-message-with-callback [figwheel-server channel-id msg-data callback]
   (-send-message figwheel-server channel-id msg-data callback))
 
+(defn eval-js
+  ([figwheel-server channel-id code]
+   (eval-js figwheel-server channel-id code nil))
+  ([figwheel-server channel-id code callback]
+   (send-message-with-callback figwheel-server channel-id {:msg-name :repl-eval :code code} callback)))
+
 (defn connection-data [figwheel-server]
   (-connection-data figwheel-server))
 
