06e696dd4850a0afacba9d02641b5902b892ce4d
Vlad Bokov <razum2um@mail.ru>
2015-10-26 13:59:22 +0600

    add eval-cljs

--- sidecar/src/figwheel_sidecar/channel_server.clj
+++ sidecar/src/figwheel_sidecar/channel_server.clj
@@ -1,4 +1,7 @@
-(ns figwheel-sidecar.channel-server)
+(ns figwheel-sidecar.channel-server
+  (:require [cljs.compiler :as comp]
+            [cljs.analyzer :as ana]
+            [clojure.edn :as edn]))
 
 (defprotocol ChannelServer
   (-send-message [this channel-id msg-data callback])
@@ -15,6 +18,18 @@
    (eval-js figwheel-server channel-id code nil))
   ([figwheel-server channel-id code callback]
    (-send-message figwheel-server channel-id {:msg-name :repl-eval :code code} callback)))
+
+(defn eval-cljs
+  ([figwheel-server channel-id code]
+   (eval-cljs figwheel-server channel-id code nil))
+  ([figwheel-server channel-id code callback]
+   (let [js-code (->> code
+                   edn/read-string
+                   (ana/analyze (ana/empty-env))
+                   ana/no-warn
+                   comp/emit
+                   with-out-str)]
+     (eval-js figwheel-server channel-id js-code))))
 
 (defn connection-data [figwheel-server]
   (-connection-data figwheel-server))
