6f96c57ea3ae2b943826d5678df465d08b99c4dc
Bruce Hauman <bhauman@gmail.com>
2015-11-09 11:28:40 -0500

    bump example clojurescript version

--- example/project.clj
+++ example/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.122"]
+                 [org.clojure/clojurescript "1.7.170"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [sablono "0.3.5"]
                  [org.omcljs/om "0.8.8"]
