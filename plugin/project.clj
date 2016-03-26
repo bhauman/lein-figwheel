360200ee714d07158acad6130570f6d31108c96c
Bruce Hauman <bhauman@gmail.com>
2016-03-25 20:16:48 -0400

    release 0.5.2

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,11 +1,11 @@
-(defproject lein-figwheel "0.5.2-SNAPSHOT"
+(defproject lein-figwheel "0.5.2"
   :description "ClojureScript Autobuilder/Server which pushes changed files to the browser. This is the lein plugin."
   :url "https://github.com/bhauman/lein-figwheel"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[figwheel-sidecar "0.5.2-SNAPSHOT"]
-                 [figwheel "0.5.2-SNAPSHOT"]]
+  :dependencies [[figwheel-sidecar "0.5.2"]
+                 [figwheel "0.5.2"]]
 
   :scm { :name "git"
          :url "https://github.com/bhauman/lein-figwheel"
