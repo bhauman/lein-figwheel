2f43975b0b80da84b431af68fb3293a2e594105b
Bruce Hauman <bhauman@gmail.com>
2016-06-05 12:49:31 -0400

    Protect against corrupt builds using a stamp and clean system
    fixed #413

--- sidecar/src/figwheel_sidecar/build_middleware/stamp_and_clean.clj
+++ sidecar/src/figwheel_sidecar/build_middleware/stamp_and_clean.clj
@@ -3,22 +3,24 @@
    [figwheel-sidecar.utils :as utils]
    [clojure.java.io :as io]))
 
-;; some minimal protection against corrupt builds
-;; this is really only intended to run once at the start
-;; of an autobuild session but its fast enough to run every time
+;; Minimal protection against corrupt builds
+
+;; While minimal this will prevent plenty of headaches caused by
+;; corrupted build :output-dir directories
+
+;; This is a temporary solution until the ClojureScript compiler supports
+;; something like a :signature key
 
 (def user-dir-hash  (.hashCode (System/getProperty "user.dir")))
 
+;; could hash all jar file names??
 (def classpath-hash (.hashCode (System/getProperty "java.class.path")))
 
 (defn stamp-file [build-config]
-  (io/file
-   (System/getProperty "java.io.tmpdir")
-   (str
-    (hash
-     {:build-id  (.hashCode (str (:id build-config)))
-      :root-path user-dir-hash})
-    ".txt")))
+  (io/file (or (System/getProperty "user.dir")
+               (io/file "."))
+           (or (-> build-config :build-options :output-dir) "out")
+           ".figwheel-compile-stamp"))
 
 ;; this provides a simple map and stable order for hashing
 (defn options-that-affect-build-cache [build-config]
@@ -38,22 +40,22 @@
        (.hashCode)
        str))
 
-#_(count (into #{}  (take 100000 (repeatedly #(current-stamp-signature t)))))
-
 (defn stamp! [build-config]
-  (println "Stamping!")
-  (println "Stamp file" (stamp-file build-config))
-  (println "Stamp" (current-stamp-signature build-config))
-  (spit (stamp-file build-config)
-        (current-stamp-signature build-config)))
+  #_(println "Stamping!")
+  #_(println "Stamp file" (stamp-file build-config))
+  #_(println "Stamp" (current-stamp-signature build-config))
+  (let [sfile (stamp-file build-config)]
+    (.mkdirs (.getParentFile sfile))
+    (spit sfile
+          (current-stamp-signature build-config))))
 
 (defn stamp-value [build-state]
   (let [sf (stamp-file build-state)]
     (when (.exists sf) (slurp sf))))
 
 (defn stamp-matches? [build-config]
-  (println "Current Stamp " (current-stamp-signature build-config))
-  (println "Previous Stamp" (stamp-value build-config))
+  #_(println "Current Stamp " (current-stamp-signature build-config))
+  #_(println "Previous Stamp" (stamp-value build-config))
   (= (current-stamp-signature build-config)
      (stamp-value build-config)))
 
