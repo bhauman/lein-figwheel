e12e06a7b04c3db31c2b154167334a3375415a34
Bruce Hauman <bhauman@gmail.com>
2016-09-27 12:55:45 -0400

    update core.async dep

--- support/project.clj
+++ support/project.clj
@@ -10,5 +10,5 @@
   [[org.clojure/clojure "1.8.0"]
    [org.clojure/clojurescript "1.8.51"
     :exclusions [org.apache.ant/ant]]
-   [org.clojure/core.async "0.2.385"
+   [org.clojure/core.async "0.2.391"
     :exclusions [org.clojure/tools.reader]]])
