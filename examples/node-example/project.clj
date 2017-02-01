596009b48795a7f85057098db14baed315f32edd
Bruce Hauman <bhauman@gmail.com>
2017-01-31 21:44:07 -0500

    update node app a bit

--- examples/node-example/project.clj
+++ examples/node-example/project.clj
@@ -9,7 +9,7 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/clojurescript "1.9.229"]]
 
-  :plugins [[lein-figwheel "0.5.9-SNAPSHOT"]]
+  :plugins [[lein-figwheel "0.5.10-SNAPSHOT"]]
 
   :source-paths ["src"]
 
@@ -27,4 +27,7 @@
                            ;; !!! need to set the target to :nodejs !!!
                            :target :nodejs}}]}
 
+  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.10-SNAPSHOT"]
+                                  [com.cemerick/piggieback "0.2.1"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
 )
