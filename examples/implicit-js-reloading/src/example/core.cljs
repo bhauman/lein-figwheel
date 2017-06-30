a61a444679ed3ba414da301e974c5fecd1251649
Mads Olsen <olsenmads@gmail.com>
2017-06-30 21:05:22 +0200

    Remove need for initialising counter in demonstration of counter increment pattern by wrapping inc with fnil. (#553)

--- examples/implicit-js-reloading/src/example/core.cljs
+++ examples/implicit-js-reloading/src/example/core.cljs
@@ -16,7 +16,7 @@
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  ;; (swap! app-state update-in [:__figwheel_counter] (fnil inc 0))
   )
 
 
