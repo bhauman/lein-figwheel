03b088103f5918e4f74da05a0f372fe736916f60
Bruce Hauman <bhauman@gmail.com>
2017-12-17 15:36:08 -0500

    remove non tests

--- support/test/figwheel/client_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns figwheel.client-test
-  (:require
-   [figwheel.client :as c]
-   [cljs.test :refer [testing is deftest]]))
-
-(deftest checking
-  (is true))
--- support/test/figwheel/file_reloading_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns figwheel.file-reloading-test
-  (:require
-   [figwheel.client.file-reloading :as f]
-   [cljs.test :refer [testing is deftest]]))
-
-(deftest checking
-  (is true))
