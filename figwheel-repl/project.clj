64090ef3d5c1b8a600187979192d433a9bf313cb
Bruce Hauman <bhauman@gmail.com>
2018-04-10 09:37:22 -0400

    clean up deps

--- figwheel-repl/project.clj
+++ figwheel-repl/project.clj
@@ -1,7 +1,7 @@
 (defproject figwheel-repl "0.1.0-SNAPSHOT"
   :description  "Figwheel REPL provides a stable multiplexing REPL for clojurescript"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.10.191"]
+                 [org.clojure/clojurescript "1.10.238"]
                  [ring/ring-core "1.6.3"]
                  [ring/ring-defaults "0.3.1"]
                  [co.deps/ring-etag-middleware "0.2.0"]
