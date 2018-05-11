bc14eaeef49920cb24e755122a6098c1326d4882
Bruce Hauman <bhauman@gmail.com>
2018-05-11 15:15:00 -0400

    figwheel.core: protect against nil values when finding hooks

--- figwheel-main/devel/exproj/core.cljs
+++ figwheel-main/devel/exproj/core.cljs
@@ -1,26 +1,40 @@
-(ns exproj.core
+(ns ^:figwheel-hooks exproj.core
   (:require
-   [goog.events]))
+   [goog.events]
+   [goog.object :as gobj]
+   [clojure.string :as string]))
 
 (enable-console-print!)
 
 (defn hello []
-  "hello exproj")
+  "hello  exproj")
 
-(prn (hello))
+(defn ^:after-load after-hook []
+  (js/console.log "Called the AFTER hook!!!"))
+
+(defn ^:before-load befor-hook [& args]
+  (js/console.log "Called the before hook!!!"))
+
+#_(d)
 
 ;; stable reference
-(defonce after-load (fn [e] (prn :after (.. e -data))))
+#_(defonce after-load (fn [e] (prn :after (.. e -data))))
 ;; idempotent with stable reference
-(.addEventListener js/document.body "figwheel.after-load" after-load)
+#_(.addEventListener js/document.body "figwheel.after-load" after-load)
+#_(cljs.pprint/pprint (deref js/figwheel.core.state))
+#_(defonce before-load (fn [e] (prn :before (.. e -data))))
+;; idempotent with stable reference
+#_(.addEventListener js/document.body "figwheel.before-load" before-load)
 
-(defonce before-load (fn [e] (prn :before (.. e -data))))
-;; idempotent with stable reference
-(.addEventListener js/document.body "figwheel.before-load" before-load)
 
-(defonce after-css-load (fn [e] (prn :after-css-load (.. e -data))))
+
+#_(defonce after-css-load (fn [e] (prn :after-css-load (.. e -data))))
 ;; idempotent with stable reference
-(.addEventListener js/document.body "figwheel.after-css-load" after-css-load)
+#_(.addEventListener js/document.body "figwheel.after-css-load" after-css-load)
+
+(defn -main [& args]
+  (prn 35)
+  35)
 
 #_(defn)
 #_(d d d d d d d)
