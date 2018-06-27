a405a65353b9063d62eb373a4e53a487d97f9f33
Bruce Hauman <bhauman@gmail.com>
2018-06-27 15:14:37 -0400

    remove figwheel-core and figwheel-repl
    new repositories have been set up
    https://github.com/bhauman/figwheel-repl
    https://github.com/bhauman/figwheel-core

--- figwheel-repl/src/cljs/repl/figwheel.clj
+++
@@ -1,6 +0,0 @@
-(ns cljs.repl.figwheel
-  (:require
-   [figwheel.repl :as fr]))
-
-(defn repl-env [& {:as opts}]
-  (fr/repl-env* opts))
--- figwheel-repl/src/figwheel/repl/preload.cljs
+++
@@ -1,6 +0,0 @@
-(ns figwheel.repl.preload
-  (:require [figwheel.repl :as fr]))
-
-(if (= fr/host-env :html)
-  (.addEventListener goog.global "load" #(fr/connect))
-  (fr/connect))
