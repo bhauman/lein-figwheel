815c0c543a9d0a13a52756444b187b3919500c4f
Bruce Hauman <bhauman@gmail.com>
2018-11-15 08:05:51 -0500

    update dev resources

--- example/project.clj
+++ example/project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.9.0-alpha15"]
-                 [org.clojure/clojurescript "1.9.908"]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.439"]
 
                  ;; trying to keep things compatible back to these versions at least
                  #_[org.clojure/clojure "1.8.0"]
